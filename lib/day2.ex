e43694d3e9cd6b22f6764466af04f862eed9e7fc
Nicholas Montaño <montanonic@gmail.com>
2019-12-05 12:51:44 -0800

    Finish part 2

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,17 +1,46 @@
 defmodule Day2 do
   def run do
-    IO.inspect(part1())
+    IO.inspect(part1(), label: "Part 1")
+    IO.inspect(part2(), label: "Part 2")
   end
 
   def part1 do
-    Day2.Implementation.get_program()
-    |> List.replace_at(1, 12)
-    |> List.replace_at(2, 2)
-    |> Day2.Implementation.run_program()
+    run_program(12, 2)
     |> hd()
   end
 
+  def part2 do
+    range = 0..99
+
+    Stream.flat_map(range, fn noun ->
+      Stream.map(range, fn verb ->
+        output =
+          run_program(noun, verb)
+          |> hd()
+
+        if output == 19_690_720 do
+          {noun, verb}
+        end
+      end)
+      |> Enum.filter(& &1)
+    end)
+    |> Enum.at(0)
+    |> (fn {noun, verb} -> 100 * noun + verb end).()
+  end
+
+  def run_program(noun, verb) do
+    Day2.Implementation.get_program()
+    |> Day2.Implementation.set_noun_and_verb(noun, verb)
+    |> Day2.Implementation.run_program()
+  end
+
   defmodule Implementation do
+    def set_noun_and_verb(program, noun, verb) do
+      program
+      |> List.replace_at(1, noun)
+      |> List.replace_at(2, verb)
+    end
+
     def get_program do
       Advent2019.input_lines(2)
       |> Enum.at(0)
